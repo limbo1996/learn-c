aa0aa295ae8371a934eb9bb9e23daaf2c81aa0bd
limbo1996 <wangxuan1@shanghaitech.edu.cn>
2019-10-11 23:47:41 +0800

    begin

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int num;/*定义一个名为num的变量 */
+    num = 1;/*给num赋值为1 */
+
+    printf("I am a simple ");
+    printf("computer. \n");
+    printf("My favorite number is %d because it is first.\n", num);
+    return 0 ;
+}
